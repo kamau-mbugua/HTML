6bd2f802df106c40a94d5cbcba216a347cd5c04f
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-11-17 10:51:23 +0300

    Content Activities

--- Downloads/Compressed/LEARN-HTML-APP-master/app/src/main/java/com/example/learnhtml/MainActivity.java
+++ Downloads/Compressed/LEARN-HTML-APP-master/app/src/main/java/com/example/learnhtml/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.learnhtml;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -50,101 +53,150 @@ public class MainActivity extends AppCompatActivity {
     private void selectDrawerItem(MenuItem item) {
         switch (item.getItemId()){
             case R.id.nav_home:
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
                 finish();
                 break;
             case R.id.nav_editor:
+                startActivity(new Intent(getApplicationContext(),EditorActivity.class));
                 finish();
                 break;
             case R.id.nav_quiz:
+                startActivity(new Intent(getApplicationContext(),ProgressActivity.class));
                 finish();
                 break;
             case R.id.nav_Introduction:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Editors:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Elements:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Attributes:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Comments:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Styles:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Colors:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Responsive:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_centered:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_BasicExample:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Headings:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Paragraphs:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Links:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_LineBreak:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_HorizontalRule:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_TextFormatting:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_BlockLevel:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Sections:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Images:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Tables:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Lists:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_DescriptionLists:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Javascript:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_Forms:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_FormLabels:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_InputTypes:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             case R.id.nav_TextArea:
+                startActivity(new Intent(getApplicationContext(),ContentActivity.class));
                 finish();
                 break;
             default:
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     private ActionBarDrawerToggle setupDrawerToggle() {
         return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
 
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
